294d07624302a928260c6beba1343f3f44a408e9
Imran Matin <imatin@amazon.com>
2024-04-19 14:30:42 -0700

    Change logging level on Flask

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s: %(message)s')
 
 @app.route('/', methods=['POST'])
 async def index():
